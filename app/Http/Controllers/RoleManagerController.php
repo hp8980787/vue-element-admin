dca7b99480e066d653075a441444c5018faf6b9e
constantine <643711690@qq.com>
2021-08-31 17:31:21 +0800

    first commit

---
+++ app/Http/Controllers/RoleManagerController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\Models\User;
+
+class RoleManager extends Controller
+{
+    public function permissionsIndex()
+    {
+        return Permission::all()->paginate(100);
+    }
+
+
+    public function rolesIndex()
+    {
+        return Role::all()->paginate(100);
+    }
+
+
+
+    public function rolesAddUser(Request $request, Role $role, User $user)
+    {
+
+        $user->assignRole($role);
+
+        return response()->json([
+            "message" => $role->name . " Role successfully assigned to User!"
+        ], 200);
+    }
+
+
+    public function rolesRemoveUser(Request $request, Role $role, User $user)
+    {
+        $user->removeRole($role);
+
+        return response()->json([
+            "message" => $role->name . " Role successfully removed from User"
+        ], 200);
+    }
+}
